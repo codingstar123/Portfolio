e71b03c8e1a2bc8f1812101efe81733a511ac628
Shufeng Wu <wu666@Shufengs-MacBook-Air.local>
2022-11-02 13:55:04 -0400

    Portfolia page is set up

--- portfolio.js
+++ portfolio.js
@@ -29,5 +29,22 @@ let currentImgIndex = 0;
     $('.carousel-images').children().eq(currentImgIndex)
       .css('display','block')
     })
+// Contact Button;
+$( () => {
 
+    //======grabbing the buttons=======
+    const openBtn = $('#openContact');
+    const contact = $('#contact');
+    const closeBtn = $('#close');
+
+    //===Event Handler===
+    function openContact (){
+      contact.css('display','block')
+    };
+    function closeContact () {
+      contact.css('display','none')
+    }
+    openBtn.on("click", openContact);
+    closeBtn.on('click',closeContact);
+    });
 });
