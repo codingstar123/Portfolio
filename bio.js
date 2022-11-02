ca162cbbaa713ec2dae7c2534acdb00cb9f67837
Shufeng Wu <wu666@Shufengs-MacBook-Air.local>
2022-11-02 15:50:25 -0400

    bio page is set

---
+++ bio.js
@@ -0,0 +1,17 @@
+$(() => {
+  //======grabbing the buttons=======
+  const openBtn = $('#openContact');
+  const contact = $('#contact');
+  const closeBtn = $('#close')
+
+  //===Event Handler===
+  function openContact(){
+    contact.css('display','block')
+  };
+  function closeContact(){
+    contact.css('display','none')
+  }
+  openBtn.on('click',openContact);
+  closeBtn.on('click',closeContact);
+
+});
